8e60a7e35351201d463d2a895637b534d275527e
HwangGwiMan <eze886@daum.net>
2026-01-16 22:25:15 +0900

    유저 정보 조회할때 Role과 Permission을 즉시 로딩(eagerly load) 하도록 변경

--- src/main/java/com/TodoApp/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/TodoApp/backend/domain/user/repository/UserRepository.java
@@ -1,16 +1,19 @@
 package com.TodoApp.backend.domain.user.repository;
 
 import com.TodoApp.backend.domain.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
-    Optional<User> findByUsername(String username);
-    
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles r LEFT JOIN FETCH r.permissions WHERE u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
+
     Optional<User> findByEmail(String email);
     
     boolean existsByUsername(String username);
